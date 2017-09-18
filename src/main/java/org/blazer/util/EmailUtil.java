40751a4981f2dd791d35b4b0dfe3b4be469e3c5f
hyy <hyy@172.16.102.118>
2017-09-18 11:00:25 +0800

    增加发送邮件Util

---
+++ src/main/java/org/blazer/util/EmailUtil.java
@@ -0,0 +1,126 @@
+package org.blazer.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimeUtility;
+
+import org.blazer.common.conf.Conf;
+import org.blazer.common.conf.ConfUtil;
+import org.blazer.common.util.PathUtil;
+
+public class EmailUtil {
+
+	public static void main(String[] args) throws IOException {
+		// args = new String[] { "25958235@qq.com,blazerhe@gmail.com",
+		// "aaaaaaa", "ccccccc" };
+		// args = new String[] { "25958235@qq.com", "你好222", "aaaaaaa" };
+		Conf conf = ConfUtil.getConf(PathUtil.root + "email.conf");
+		if (conf.isEmpty()) {
+			conf = ConfUtil.getConf("/email.conf");
+		}
+		System.out.println(PathUtil.root + "email.conf");
+		System.out.println(conf.toString());
+		if (args.length < 3) {
+			System.err.println("Usage: java -jar xxx.jar ${toMail} ${subject} ${content} [${filePath_1} ${filePath_2} ...]");
+		}
+		EmailUtil.config(conf.get("fromMail"), conf.get("fromMailCN"), conf.get("pwd"), conf.get("host"));
+		boolean flag = EmailUtil.sendMail(args[0], args[1], args[2], args.length > 3 ? args[3].split(",") : null);
+		if (flag) {
+			System.out.println("发送成功！");
+			System.exit(0);
+		} else {
+			System.err.println("发送失败！");
+			System.exit(-1);
+		}
+	}
+
+	// 发件邮箱
+	private static String fromMail = "blazerhehe@163.com";
+	// 发件名称
+	private static String fromMailCN = "测试服务器";
+	// 密码
+	private static String pwd = "blazer222";
+	// 服务器
+	private static String host = "smtp.163.com";
+
+	public static void config(String fromMail, String fromMailCN, String pwd, String host) {
+		EmailUtil.fromMail = fromMail;
+		EmailUtil.fromMailCN = fromMailCN;
+		EmailUtil.pwd = pwd;
+		EmailUtil.host = host;
+	}
+
+	public static boolean sendMail(String toMail, String subject, String content, String... filePaths) {
+		System.out.println("收件人：" + toMail);
+		System.out.println("主题：" + subject);
+		System.out.println("内容：" + "<xmp>" + content + "</xmp>");
+		if (filePaths != null) {
+			for (int i = 0; i < filePaths.length; i++) {
+				System.out.println("附件" + (i + 1) + "：" + filePaths[i]);
+			}
+		}
+		boolean flag = false;
+		// 端口
+		int port = 25;
+		try {
+			Properties props = new Properties();
+			props.put("mail.smtp.host", host);
+			props.put("mail.smtp.auth", "true");
+			Session session = Session.getDefaultInstance(props);
+			session.setDebug(false);
+			MimeMessage message = new MimeMessage(session);
+			/* 标题 */
+			message.setFrom(new InternetAddress(fromMail, fromMailCN));
+			/* 发送给多人，用 逗号","分隔。 */
+			for (String toMailOne : toMail.split(",")) {
+				message.addRecipient(Message.RecipientType.TO, new InternetAddress(toMailOne));
+			}
+			message.setSubject(subject);
+			message.addHeader("charset", "UTF-8");
+
+			/* 添加正文内容 */
+			Multipart multipart = new MimeMultipart();
+			BodyPart contentPart = new MimeBodyPart();
+			contentPart.setText(content);
+			contentPart.setHeader("Content-Type", "text/html; charset=UTF-8");
+			multipart.addBodyPart(contentPart);
+
+			/* 添加附件 */
+			if (filePaths != null) {
+				for (String filePath : filePaths) {
+					File uploadFile = new File(filePath);
+					MimeBodyPart fileBody = new MimeBodyPart();
+					fileBody.setDataHandler(new DataHandler(new FileDataSource(uploadFile)));
+					fileBody.setFileName(MimeUtility.encodeText(uploadFile.getName()));
+					multipart.addBodyPart(fileBody);
+				}
+			}
+			message.setContent(multipart);
+			message.setSentDate(new Date());
+			message.saveChanges();
+			Transport transport = session.getTransport("smtp");
+			transport.connect(host, port, fromMail, pwd);
+			transport.sendMessage(message, message.getAllRecipients());
+			transport.close();
+			flag = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
+	}
+
+}
---
+++ src/main/java/org/blazer/util/EmailUtil_Csv2HtmlTable.java
@@ -0,0 +1,82 @@
+package org.blazer.util;
+
+import java.io.IOException;
+
+import org.blazer.common.conf.Conf;
+import org.blazer.common.conf.ConfUtil;
+import org.blazer.common.file.FClassReader;
+import org.blazer.common.file.FHandler;
+import org.blazer.common.file.FReader;
+import org.blazer.common.util.PathUtil;
+
+public class EmailUtil_Csv2HtmlTable {
+
+	public static void main(String[] args) throws IOException {
+		// args = new String[] { "25958235@qq.com", "你好222", "aaaaaaa" };
+		Conf conf = ConfUtil.getConf(PathUtil.root + "email.conf");
+		if (conf.isEmpty()) {
+			conf = ConfUtil.getConf("/email.conf");
+		}
+		System.out.println(PathUtil.root + "email.conf");
+		System.out.println(conf.toString());
+		if (args.length < 3) {
+			System.err.println("Usage: java -jar xxx.jar ${toMail} ${subject} ${content} ${csvFilePath} [${filePath_1} ${filePath_2} ...]");
+		}
+		EmailUtil.config(conf.get("fromMail"), conf.get("fromMailCN"), conf.get("pwd"), conf.get("host"));
+
+		final StringBuilder content = new StringBuilder();
+		FClassReader.create("/email_template.html", new FHandler() {
+			@Override
+			public void handle(String row) throws IOException {
+				content.append(row);
+			}
+		});
+		final StringBuilder moreThan100 = new StringBuilder("false");
+		content.append("<p>").append(args[2]).append("</p>");
+		content.append("<table>");
+		FReader.create(args[3], new FHandler() {
+			@Override
+			public void handle(String row) throws IOException {
+				if (index() >= 100) {
+					moreThan100.setLength(0);
+					moreThan100.append("true");
+					return;
+				}
+				String[] strs = row.split("\t");
+				if (index() == 0) {
+					content.append("<thead><tr>");
+					for (String str : strs) {
+						content.append("<th>").append(str).append("</th>");
+					}
+					content.append("</tr></thead>");
+				} else {
+					if (index() == 1) {
+						content.append("<tbody>");
+					}
+					content.append("<tr>");
+					for (String str : strs) {
+						content.append("<td>").append(str).append("</td>");
+					}
+					content.append("</tr>");
+				}
+			}
+		});
+		content.append("</tbody>");
+		content.append("</table>");
+		// 大于100行
+		if (Boolean.parseBoolean(moreThan100.toString())) {
+			content.append("<h3>结果行数大于100，不予以展示，请查收附件！</h3>");
+		} else {
+			content.append("<h4>结果展示完毕！</h4>");
+		}
+		boolean flag = EmailUtil.sendMail(args[0], args[1], content.toString(), args.length > 3 ? args[3].split(",") : null);
+		if (flag) {
+			System.out.println("发送成功！");
+			System.exit(0);
+		} else {
+			System.err.println("发送失败！");
+			System.exit(-1);
+		}
+	}
+
+}
